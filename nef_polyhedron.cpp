65a6a3c3e174a6a6ce8a57e7f35aa4664e7eae89
Nicholas Gill <mythagel@gmail.com>
2013-04-21 10:59:11 +1000

    interface changes to allow glide operation

--- nef_polyhedron.cpp
+++ nef_polyhedron.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "nef_polyhedron.h"
+#include <istream>
+#include <ostream>
 
 //#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 //#include <CGAL/Nef_polyhedron_3.h>
@@ -19,8 +21,11 @@
 //typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 //typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
 
+#include <CGAL/minkowski_sum_3.h>
+
 typedef CGAL::Extended_homogeneous<CGAL::Gmpz> Kernel;
 typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
+typedef Kernel::Point_3 Point_3;
 
 struct nef_polyhedron_t::private_t
 {
@@ -31,16 +36,6 @@ struct nef_polyhedron_t::private_t
 	}
 	Nef_polyhedron_3 nef;
 };
-
-std::ostream& operator<<(std::ostream& os, const nef_polyhedron_t& poly)
-{
-	return os << poly.priv->nef;
-}
-std::istream& operator>>(std::istream& is, nef_polyhedron_t& poly)
-{
-	poly.ensure_unique();
-	return is >> poly.priv->nef;
-}
 
 nef_polyhedron_t::nef_polyhedron_t(const std::shared_ptr<private_t>& priv)
  : priv(priv)
@@ -56,12 +51,6 @@ void nef_polyhedron_t::ensure_unique()
 nef_polyhedron_t::nef_polyhedron_t()
  : priv(std::make_shared<private_t>())
 {
-}
-
-nef_polyhedron_t& nef_polyhedron_t::operator=(const nef_polyhedron_t& poly)
-{
-	priv = std::make_shared<private_t>(*poly.priv);
-	return *this;
 }
 
 nef_polyhedron_t nef_polyhedron_t::operator*(const nef_polyhedron_t& poly) const
@@ -134,7 +123,42 @@ bool nef_polyhedron_t::operator>=(const nef_polyhedron_t& poly) const
 	return priv->nef >= poly.priv->nef;
 }
 
-nef_polyhedron_t::~nef_polyhedron_t()
+nef_polyhedron_t nef_polyhedron_t::glide(/*polyline*/) const
 {
+	/*
+	 * Need to convert to different kernel before glide operation
+	 */
+//	typedef Point_3* point_iterator;
+//	typedef std::pair<point_iterator,point_iterator>  point_range;
+//	typedef std::list<point_range> polyline;
+//
+//	Point_3 pl[] = { Point_3(-100, 0, 0), Point_3(40, -70, 0), Point_3(40, 50, 40), Point_3(-90, -60, 60), Point_3(0, 0, -100), Point_3(30, 0, 150) };
+//
+//	polyline poly;
+//	poly.push_back(point_range(pl, pl + 6));
+//
+//	auto glided_priv = std::make_shared<private_t>();
+//
+//	Nef_polyhedron_3 N1(poly.begin(), poly.end(), Nef_polyhedron_3::Polylines_tag());
+//	glided_priv->nef = CGAL::minkowski_sum_3(priv->nef, N1);
+//
+//	if (glided_priv->nef.is_simple())
+//	{
+//		return { glided_priv };
+//	}
+//	else
+	{
+		// TODO result is not a 2-manifold. what now?
+		return {};
+	}
 }
 
+std::ostream& operator<<(std::ostream& os, const nef_polyhedron_t& poly)
+{
+	return os << poly.priv->nef;
+}
+std::istream& operator>>(std::istream& is, nef_polyhedron_t& poly)
+{
+	poly.ensure_unique();
+	return is >> poly.priv->nef;
+}
--- nef_polyhedron.h
+++ nef_polyhedron.h
@@ -7,14 +7,13 @@
 
 #ifndef NEF_POLYHEDRON_H_
 #define NEF_POLYHEDRON_H_
-#include <istream>
-#include <ostream>
+#include <iosfwd>
 #include <memory>
 
-class nef_polyhedron_t;
-std::ostream& operator<<(std::ostream& os, const nef_polyhedron_t& poly);
-std::istream& operator>>(std::istream& is, nef_polyhedron_t& poly);
-
+/*
+ * Wrapper class for CGAL::Nef_polyhedron_3 to avoid long build times.
+ * Intention is to add higher level interfaces (i.e. volume calculation)
+ */
 class nef_polyhedron_t
 {
 private:
@@ -23,12 +22,14 @@ private:
 	nef_polyhedron_t(const std::shared_ptr<private_t>& priv);
 	void ensure_unique();
 
-	friend std::ostream& operator<<(std::ostream& os, const nef_polyhedron_t& poly);
-	friend std::istream& operator>>(std::istream& is, nef_polyhedron_t& poly);
+	friend std::ostream& operator<<(std::ostream&, const nef_polyhedron_t&);
+	friend std::istream& operator>>(std::istream&, nef_polyhedron_t&);
 public:
 	nef_polyhedron_t();
-
-	nef_polyhedron_t& operator=(const nef_polyhedron_t& poly);
+	nef_polyhedron_t(const nef_polyhedron_t&) = default;
+	nef_polyhedron_t(nef_polyhedron_t&&) = default;
+	nef_polyhedron_t& operator=(const nef_polyhedron_t&) = default;
+	nef_polyhedron_t& operator=(nef_polyhedron_t&&) = default;
 
 	nef_polyhedron_t operator*(const nef_polyhedron_t& poly) const;
 	nef_polyhedron_t operator+(const nef_polyhedron_t& poly) const;
@@ -47,7 +48,12 @@ public:
 	bool operator<=(const nef_polyhedron_t& poly) const;
 	bool operator>=(const nef_polyhedron_t& poly) const;
 
-	~nef_polyhedron_t();
+	nef_polyhedron_t glide(/*polyline*/) const;
+
+	~nef_polyhedron_t() = default;
 };
+
+std::ostream& operator<<(std::ostream&, const nef_polyhedron_t&);
+std::istream& operator>>(std::istream&, nef_polyhedron_t&);
 
 #endif /* NEF_POLYHEDRON_H_ */
