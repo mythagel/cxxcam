ff542a761e0d0079a65cacef3ce970c3782a16d4
Nicholas Gill <mythagel@gmail.com>
2013-04-22 22:03:51 +1000

    Formatting + c++11ify

--- nef_polyhedron.cpp
+++ nef_polyhedron.cpp
@@ -40,6 +40,7 @@ typedef CGAL::Extended_homogeneous<CGAL::Gmpz> Exact_Kernel;
 //#include <CGAL/Polyhedron_copy_3.h>
 // soo....
 #include <CGAL/Modifier_base.h>
+#include <CGAL/Inverse_index.h>
 #include <CGAL/Polyhedron_incremental_builder_3.h>
 
 typedef CGAL::Nef_polyhedron_3<Exact_Kernel> Nef_polyhedron_3;
@@ -67,66 +68,62 @@ void copy_to(const Poly_A& poly_a, Poly_B& poly_b)
 #else
 template <class Polyhedron_input, class Polyhedron_output>
 struct Copy_polyhedron_to
-  : public CGAL::Modifier_base<typename Polyhedron_output::HalfedgeDS>
+ : public CGAL::Modifier_base<typename Polyhedron_output::HalfedgeDS>
 {
-  Copy_polyhedron_to(const Polyhedron_input& in_poly)
-    : in_poly(in_poly) {}
+	Copy_polyhedron_to(const Polyhedron_input& in_poly)
+	 : in_poly(in_poly)
+	{
+	}
 
-  void operator()(typename Polyhedron_output::HalfedgeDS& out_hds)
-  {
-    typedef typename Polyhedron_output::HalfedgeDS Output_HDS;
-    typedef typename Polyhedron_input::HalfedgeDS Input_HDS;
+	void operator()(typename Polyhedron_output::HalfedgeDS& out_hds)
+	{
+		typedef typename Polyhedron_output::HalfedgeDS Output_HDS;
+		typedef typename Polyhedron_input::HalfedgeDS Input_HDS;
 
-    CGAL::Polyhedron_incremental_builder_3<Output_HDS> builder(out_hds);
+		CGAL::Polyhedron_incremental_builder_3<Output_HDS> builder(out_hds);
 
-    typedef typename Polyhedron_input::Vertex_const_iterator Vertex_const_iterator;
-    typedef typename Polyhedron_input::Facet_const_iterator  Facet_const_iterator;
-    typedef typename Polyhedron_input::Halfedge_around_facet_const_circulator HFCC;
+		typedef typename Polyhedron_input::Vertex_const_iterator Vertex_const_iterator;
+		typedef typename Polyhedron_input::Facet_const_iterator  Facet_const_iterator;
+		typedef typename Polyhedron_input::Halfedge_around_facet_const_circulator HFCC;
 
-    builder.begin_surface(in_poly.size_of_vertices(),
-      in_poly.size_of_facets(),
-      in_poly.size_of_halfedges());
+		builder.begin_surface(in_poly.size_of_vertices(), in_poly.size_of_facets(), in_poly.size_of_halfedges());
 
-    for(Vertex_const_iterator
-      vi = in_poly.vertices_begin(), end = in_poly.vertices_end();
-      vi != end ; ++vi)
-    {
-      typename Polyhedron_output::Point_3 p(::CGAL::to_double( vi->point().x()),
-	::CGAL::to_double( vi->point().y()),
-	::CGAL::to_double( vi->point().z()));
-      builder.add_vertex(p);
-    }
+		for(auto vi = in_poly.vertices_begin(); vi != in_poly.vertices_end(); ++vi)
+		{
+			typename Polyhedron_output::Point_3 p(
+					::CGAL::to_double( vi->point().x()),
+					::CGAL::to_double( vi->point().y()),
+					::CGAL::to_double( vi->point().z()));
+			builder.add_vertex(p);
+		}
 
-    typedef CGAL::Inverse_index<Vertex_const_iterator> Index;
-    Index index( in_poly.vertices_begin(), in_poly.vertices_end());
+		typedef CGAL::Inverse_index<Vertex_const_iterator> Index;
+		Index index(in_poly.vertices_begin(), in_poly.vertices_end());
 
-    for(Facet_const_iterator
-      fi = in_poly.facets_begin(), end = in_poly.facets_end();
-      fi != end; ++fi)
-    {
-      HFCC hc = fi->facet_begin();
-      HFCC hc_end = hc;
-      //     std::size_t n = circulator_size( hc);
-      //     CGAL_assertion( n >= 3);
-      builder.begin_facet ();
-      do {
-	builder.add_vertex_to_facet(index[hc->vertex()]);
-	++hc;
-      } while( hc != hc_end);
-      builder.end_facet();
-    }
-    builder.end_surface();
-  } // end operator()(..)
+		for(auto fi = in_poly.facets_begin(); fi != in_poly.facets_end(); ++fi)
+		{
+			HFCC hc = fi->facet_begin();
+			HFCC hc_end = hc;
+			builder.begin_facet();
+			do
+			{
+				builder.add_vertex_to_facet(index[hc->vertex()]);
+				++hc;
+			} while(hc != hc_end);
+			builder.end_facet();
+		}
+		builder.end_surface();
+	}
 private:
-  const Polyhedron_input& in_poly;
+	const Polyhedron_input& in_poly;
 };
 
 template <class Poly_A, class Poly_B>
 void copy_to(const Poly_A& poly_a, Poly_B& poly_b)
 {
-  Copy_polyhedron_to<Poly_A, Poly_B> modifier(poly_a);
-  poly_b.delegate(modifier);
-  CGAL_assertion(poly_b.is_valid());
+	Copy_polyhedron_to<Poly_A, Poly_B> modifier(poly_a);
+	poly_b.delegate(modifier);
+	CGAL_assertion(poly_b.is_valid());
 }
 #endif
 
