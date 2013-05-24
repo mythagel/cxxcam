235a4f28fbb8425fecbff8b6f9a7c4d48dc03e09
Nicholas Gill <mythagel@gmail.com>
2013-05-24 21:30:02 +1000

    Minimal test that reproduces crash.

---
+++ test/model.cpp
@@ -0,0 +1,12 @@
+#include "Machine.h"
+#include "Stock.h"
+#include "nef/io.h"
+
+int main()
+{
+	Machine m(Machine::Type::Mill, "LinuxCNC");
+	auto stock = m.GetStock();
+	auto stock_object = to_object(stock.Model);
+	return 0;
+}
+
