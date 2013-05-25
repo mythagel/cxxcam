fcc74530d7b26e6d51fccdfc3a71b74009d30d80
Nicholas Gill <mythagel@gmail.com>
2013-05-25 13:30:37 +1000

    Correct expected failure

--- test/nef_cube2.cpp
+++ test/nef_cube2.cpp
@@ -8,9 +8,18 @@ using namespace nef;
 int main()
 {
 	// Not 2-manifold...
-	auto x1 = make_box(0, 0, 0, 1, 1, 1);
-	auto x2 = make_box(1, 1, 1, 2, 2, 2);
-	auto x = x1 + x2;
-	write_off(std::cout, x);
-	return 0;
+	try
+	{
+		auto x1 = make_box(0, 0, 0, 1, 1, 1);
+		auto x2 = make_box(1, 1, 1, 2, 2, 2);
+		auto x = x1 + x2;
+		write_off(std::cout, x);
+	}
+	catch(const std::exception& ex)
+	{
+		std::cerr << ex.what() << '\n';
+		return 0;
+	}
+	return 1;
 }
+
