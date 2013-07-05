abbf426b5e56f5e3201fa5338a6e7ab0f3a5e470
mythagel <mythagel@gmail.com>
2013-07-05 16:41:28 +1000

    Testing

--- test/path.cpp
+++ test/path.cpp
@@ -1,6 +1,32 @@
 #include "Path.h"
 #include <iostream>
 
+struct euler_rot
+{
+	double x;
+	double y;
+	double z;
+	double a;
+};
+
+euler_rot aa(const cxxcam::path::step::quaternion_t& q)
+{
+	euler_rot r;
+	auto scale = sqrt(q.R_component_1() * q.R_component_1() + q.R_component_2() * q.R_component_2() + q.R_component_3() * q.R_component_3());
+	r.x = q.R_component_1() / scale;
+	r.y = q.R_component_2() / scale;
+	r.z = q.R_component_3() / scale;
+	r.a = acos(q.R_component_4()) * 2.0;
+	
+	return r;
+}
+
+std::ostream& operator<<(std::ostream& os, const euler_rot& r)
+{
+	os << "(" << r.x << "," << r.y << "," << r.z << "," << r.a << ")";
+	return os;
+}
+
 int main()
 {
 	using namespace cxxcam;
@@ -18,7 +44,11 @@ int main()
 	auto steps = expand_linear(start, end, geometry);
 	
 	for(const auto& step : steps)
+	{
 		std::cout << step << '\n';
+		
+		std::cout << aa(step.orientation) << '\n';
+	}
 	
 	return 0;
 }
