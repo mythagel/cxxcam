70c3a07fa2f44f0ef7d4448cf39411c1d078e479
mythagel <mythagel@gmail.com>
2013-07-17 16:14:49 +1000

    Simulation work.

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -23,20 +23,37 @@
  */
 
 #include "Simulation.h"
+#include "nef/translate.h"
+#include "nef/ops.h"
+
+// TESTING CRAP
+#include "nef/io.h"
+#include <iostream>
 
 namespace cxxcam
 {
 namespace simulation
 {
 
-/*
- * 
- */
 step simulate_cut(const path::step& s0, const path::step& s1, state& s)
 {
-	auto tool = s.tool;
-	const auto orientation = s1.orientation;
+	using units::length_mm;
 	
+	auto tool = s.tool.Model();
+	const auto& o0 = s0.orientation;
+	const auto& p0 = s0.position;
+	
+	const auto& o1 = s1.orientation;
+	const auto& p1 = s1.position;
+	
+	auto t0 = nef::translate(nef::rotate(tool, o0.R_component_1(), o0.R_component_2(), o0.R_component_3(), o0.R_component_4()), length_mm(p0.x).value(), length_mm(p0.y).value(), length_mm(p0.z).value());
+//	auto t1 = nef::translate(nef::rotate(tool, o1.R_component_1(), o1.R_component_2(), o1.R_component_3(), o1.R_component_4()), p1.x, p1.y, p1.z);
+
+	nef::polyline_t path{ { {length_mm(p0.x).value(), length_mm(p0.y).value(), length_mm(p0.z).value()}, {length_mm(p1.x).value(), length_mm(p1.y).value(), length_mm(p1.z).value()} } };
+	auto tool_path = nef::glide(t0, path);
+	
+	// TESTING CRAP
+	nef::write_off(std::cout, tool_path);
 	
 	return {};
 }
