e04e618167e16f4122a42b3847ae886c631d6937
mythagel <mythagel@gmail.com>
2013-07-18 16:17:59 +1000

    Refactor and disable non-manifold test.
    Will be enabled later as investigation continues.

--- test/nef_explode.cpp
+++ test/nef_explode.cpp
@@ -8,20 +8,43 @@
 
 using namespace nef;
 
-int main()
+void simple()
+{
+	auto x1 = make_box(0, 0, 0, 1, 1, 1);
+	auto x2 = make_box(1.5, 1.5, 1.5, 2, 2, 2);
+	auto x = x1 + x2;
+	
+	auto parts = explode(x);
+	for(size_t i = 0; i < parts.size(); ++i)
+	{
+		std::ostringstream name;
+		name << "nef_explode-simple-" << i << ".off";
+		std::ofstream os(name.str());
+		write_off(os, parts[i]);
+	}
+}
+
+void nonmanifold()
 {
 	auto x1 = make_box(0, 0, 0, 1, 1, 1);
 	auto x2 = make_box(1, 1, 1, 2, 2, 2);
 	auto x = x1 + x2;
 	
-	auto parts = nef::explode(x);
+	auto parts = explode(x);
 	for(size_t i = 0; i < parts.size(); ++i)
 	{
 		std::ostringstream name;
-		name << "nef_explode-" << i << ".off";
+		name << "nef_explode-nonmanifold-" << i << ".off";
 		std::ofstream os(name.str());
-		nef::write_off(os, parts[i]);
+		write_off(os, parts[i]);
 	}
+}
+
+int main()
+{
+	simple();
+	// TODO
+	//nonmanifold();
 	return 0;
 }
 
