5247e14f782a7f5bd7fa7bedf4c73743bfa0015c
mythagel <mythagel@gmail.com>
2013-07-19 16:20:06 +1000

    Correct inverse time test
    Tool loaded is a prerequisite for Machining operations.

--- test/inverse_time.cpp
+++ test/inverse_time.cpp
@@ -8,6 +8,22 @@ using namespace cxxcam;
 int main()
 {
 	Machine m(Machine::Type::Mill);
+	
+	{
+		auto end_mill = Tool::Mill{};
+		end_mill.type = Tool::Mill::Type::End;
+		end_mill.center_cutting = false;
+		end_mill.flutes = 2;
+		end_mill.flute_length = 10;
+		end_mill.core_diameter = 0.25;
+		end_mill.cutting_length = 10;
+		end_mill.mill_diameter = 1;
+		end_mill.shank_diameter = 3;
+		end_mill.length = 30;
+		m.AddTool(5, Tool("1mm End Mill", end_mill));
+	}
+	m.ToolChange(5);
+	
 	m.SetFeedRateMode(Machine::FeedRateMode::InverseTime);
 	m.StartSpindle(300);
 	
