f8a7fa23857e771c32119b49574d3f4544cc9c2d
mythagel <mythagel@gmail.com>
2013-08-01 10:23:22 +1000

    Implement basic bounding box.

---
+++ src/Bbox.cpp
@@ -0,0 +1,102 @@
+/* cxxcam - C++ CAD/CAM driver library.
+ * Copyright (C) 2013  Nicholas Gill
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Bbox.cpp
+ *
+ *  Created on: 2013-08-01
+ *      Author: nicholas
+ */
+
+#include "Bbox.h"
+#include <algorithm>
+#include <numeric>
+#include <ostream>
+
+namespace cxxcam
+{
+
+auto to_tuple(const Bbox& b) -> decltype(std::tie(b.min, b.max))
+{
+	return std::tie(b.min, b.max);
+}
+
+Bbox::Bbox(const math::point_3& min, const math::point_3& max)
+ : min(min), max(max)
+{
+}
+
+bool Bbox::operator==(const Bbox& b) const
+{
+	return to_tuple(*this) == to_tuple(b);
+}
+bool Bbox::operator!=(const Bbox& b) const
+{
+	return to_tuple(*this) != to_tuple(b);
+}
+
+Bbox Bbox::operator+(const Bbox& b) const
+{
+	math::point_3 nmin { std::min(min.x, b.min.x), std::min(min.y, b.min.y), std::min(min.z, b.min.z) };
+	math::point_3 nmax { std::max(max.x, b.max.x), std::max(max.y, b.max.y), std::max(max.z, b.max.z) };
+	return { nmin, nmax };
+}
+Bbox& Bbox::operator+=(const Bbox& b)
+{
+	min.x = std::min(min.x, b.min.x);
+	min.y = std::min(min.y, b.min.y);
+	min.z = std::min(min.z, b.min.z);
+	max.x = std::max(max.x, b.max.x);
+	max.y = std::max(max.y, b.max.y);
+	max.z = std::max(max.z, b.max.z);
+	
+	return *this;
+}
+Bbox Bbox::operator+(const math::point_3& p) const
+{
+	math::point_3 nmin { std::min(min.x, p.x), std::min(min.y, p.y), std::min(min.z, p.z) };
+	math::point_3 nmax { std::max(max.x, p.x), std::max(max.y, p.y), std::max(max.z, p.z) };
+	return { nmin, nmax };
+}
+Bbox& Bbox::operator+=(const math::point_3& p)
+{
+	min.x = std::min(min.x, p.x);
+	min.y = std::min(min.y, p.y);
+	min.z = std::min(min.z, p.z);
+	max.x = std::max(max.x, p.x);
+	max.y = std::max(max.y, p.y);
+	max.z = std::max(max.z, p.z);
+	
+	return *this;
+}
+
+Bbox construct(const std::vector<math::point_3>& points)
+{
+	if(points.empty())
+		return {};
+		
+	return std::accumulate(begin(points), end(points), Bbox { points.front(), points.front() });
+}
+
+std::ostream& operator<<(std::ostream& os, const Bbox& b)
+{
+	os << "min: {" << b.min << "} max: {" << b.max << "}";
+	return os;
+}
+
+}
+
---
+++ src/Bbox.h
@@ -0,0 +1,57 @@
+/* cxxcam - C++ CAD/CAM driver library.
+ * Copyright (C) 2013  Nicholas Gill
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Bbox.h
+ *
+ *  Created on: 2013-08-01
+ *      Author: nicholas
+ */
+
+#ifndef BBOX_H_
+#define BBOX_H_
+#include "Math.h"
+#include <iosfwd>
+#include <vector>
+
+namespace cxxcam
+{
+
+struct Bbox
+{
+	math::point_3 min;
+	math::point_3 max;
+	
+	Bbox() = default;
+	Bbox(const math::point_3& min, const math::point_3& max);
+	
+	bool operator==(const Bbox& b) const;
+	bool operator!=(const Bbox& b) const;
+	
+	Bbox operator+(const Bbox& b) const;
+	Bbox& operator+=(const Bbox& b);
+	Bbox operator+(const math::point_3& p) const;
+	Bbox& operator+=(const math::point_3& p);
+};
+
+Bbox construct(const std::vector<math::point_3>& points);
+
+std::ostream& operator<<(std::ostream& os, const Bbox&);
+
+}
+
+#endif /* BBOX_H_ */
