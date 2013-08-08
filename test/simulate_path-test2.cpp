659807875262b348c7c8ea3dea9e16787dce97e7
mythagel <mythagel@gmail.com>
2013-08-08 15:28:07 +1000

    Modifications to investigate non-manifold error.

--- test/simulate_path-test2.cpp
+++ test/simulate_path-test2.cpp
@@ -4,6 +4,7 @@
 #include "Units.h"
 #include "geom/primitives.h"
 #include "geom/io.h"
+#include "geom/ops.h"
 #include <iostream>
 
 #include <vector>
@@ -11,9 +12,39 @@
 #include <sstream>
 #include "fold_adjacent.h"
 
+using namespace cxxcam;
+
+geom::polyhedron_t remove_material_alt(const geom::polyhedron_t& tool, const geom::polyhedron_t& stock, const std::vector<path::step>& steps)
+{
+	// TODO new design can be parallelised
+	std::vector<geom::polyhedron_t> tool_motion;
+	fold_adjacent(begin(steps), end(steps), std::back_inserter(tool_motion), 
+	[&tool](const path::step& s0, const path::step& s1) -> geom::polyhedron_t
+	{
+		return simulation::sweep_tool(tool, s0, s1);
+	});
+	
+	auto tool_path = geom::merge(tool_motion);
+	try
+	{
+		return stock - tool_path;
+	}
+	catch(...)
+	{
+		{
+			std::ofstream os("simulate_path-test2-stock.off");
+			os << geom::format::off << stock;
+		}
+		{
+			std::ofstream os("simulate_path-test2-tool_path.off");
+			os << geom::format::off << tool_path;
+		}
+		throw;
+	}
+}
+
 int main()
 {
-	using namespace cxxcam;
 	using namespace cxxcam::simulation;
 	
 	std::vector<path::step> steps;
@@ -38,10 +69,8 @@ int main()
 	for(auto step : steps)
 		std::cout << step << '\n';
 	
-	// Configure simulation
-	simulation::simulation_t sim;
-	sim.steps.path = steps;
-	sim.stock.Model = geom::make_box({0, 0, 0}, {50, 50, 100});
+	auto raw_stock = geom::make_box({0, 0, 0}, {50, 50, 100});
+	geom::polyhedron_t tool;
 	{
 		auto end_mill = Tool::Mill{};
 		end_mill.type = Tool::Mill::Type::End;
@@ -53,15 +82,13 @@ int main()
 		end_mill.mill_diameter = 10;
 		end_mill.shank_diameter = 10;
 		end_mill.length = 60;
-		sim.tool = Tool("10mm End Mill", end_mill);
+		tool = Tool("10mm End Mill", end_mill).Model();
 	}
 	
-	auto result = run(sim);
+	auto stock = remove_material_alt(tool, raw_stock, steps);
 	
-	std::cout << "Bbox: " << result.bounding_box << '\n';
-
 	std::ofstream os("simulate_path-test2.off");
-	os << geom::format::off << result.stock.Model;
+	os << geom::format::off << stock;
 	return 0;
 }
 
